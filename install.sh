36f2531fd1d959c7450b0fb5a1a2864dea33161e
CL Dev <cldev@CL-MMA5006.local>
2016-09-15 20:33:01 -0400

    update

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ curl -L https://bit.ly/janus-bootstrap | bash
 brew install coreutils
 brew install watch
 brew install tmux
+brew install ack
 
 # zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
